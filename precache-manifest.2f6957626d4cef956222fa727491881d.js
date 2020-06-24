222fd2338242e03d3a0cf9b79b78ce72c947e9f8
pongp1 <pongp1@student.op.ac.nz>
2020-06-25 10:51:19 +1200

    Updates

---
+++ precache-manifest.2f6957626d4cef956222fa727491881d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b14023b107769c5c41acda9e672dd64",
+    "url": "/Tonight-Recipe/index.html"
+  },
+  {
+    "revision": "27c83eebdde9542bc9f1",
+    "url": "/Tonight-Recipe/static/css/main.8a140bea.chunk.css"
+  },
+  {
+    "revision": "4acb93a9f64563e4bc62",
+    "url": "/Tonight-Recipe/static/js/2.a0f1ecc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tonight-Recipe/static/js/2.a0f1ecc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c83eebdde9542bc9f1",
+    "url": "/Tonight-Recipe/static/js/main.4077b6c3.chunk.js"
+  },
+  {
+    "revision": "73341170f82cbc360df9",
+    "url": "/Tonight-Recipe/static/js/runtime-main.6abe4b05.js"
+  },
+  {
+    "revision": "f3fbadd19aa3d565a6b3b37450fa8d1c",
+    "url": "/Tonight-Recipe/static/media/error.f3fbadd1.gif"
+  },
+  {
+    "revision": "cbc07a4e0665b84e4c9f95432251184c",
+    "url": "/Tonight-Recipe/static/media/loading.cbc07a4e.gif"
+  },
+  {
+    "revision": "a1f97ab190906833246a6a5174c9e954",
+    "url": "/Tonight-Recipe/static/media/logo.a1f97ab1.png"
+  }
+]);

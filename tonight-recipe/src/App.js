33e33893866fde444ef302e33d7b7811221a856d
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-08-03 15:22:22 +1200

    changing rending logic

--- tonight-recipe/src/App.js
+++ tonight-recipe/src/App.js
@@ -9,7 +9,7 @@ import loading from './image/loading.gif'
 import logo from './image/logo.png'
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       data: [],
@@ -24,32 +24,32 @@ export default class App extends React.Component {
   }
 
   //fetching data from Edamam API
-  fetchData(){
+  fetchData() {
     this.setState({
       searching: true
     });
     fetch(`https://api.edamam.com/search?q=${this.state.searchValue}&app_id=${this.app_id}&app_key=${this.app_key}`)
-    .then((res) => res.json())
-    .then((data) => {
-      this.setState({
-        data: data.hits,
-        searching: false
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          data: data.hits,
+          searching: false
+        });
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
   }
 
   //handles search field change
-  handleChange(e){
+  handleChange(e) {
     this.setState({
       searchValue: e.target.value
     });
   }
 
   //handles the search button being clicked
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
     //fetch the data when the search button is clicked
     this.fetchData();
@@ -60,7 +60,7 @@ export default class App extends React.Component {
   }
 
   //handles the recipe card being clicked
-  handleRecipeClick(popupState, recipeData){
+  handleRecipeClick(popupState, recipeData) {
     //recipeData - data of the recipe that was clicked
     this.setState({
       showPopup: popupState,
@@ -68,10 +68,11 @@ export default class App extends React.Component {
     });
   }
 
-  render(){
-    if(this.state.data.length === 0){
+  render() {
+    //if there is no recipe data and data is not currently being fetch then display homepage
+    if (this.state.data.length === 0 && this.state.searching === false) {
       return (
-        <div>
+        <div className={styles.appContainer}>
           <p className={styles.mainTitle}>Tonight's Recipe</p>
           <p className={styles.subTitle}>Food ideas just a click away</p>
           {/*image design by Amy Cleaver*/}
@@ -82,73 +83,47 @@ export default class App extends React.Component {
             searchValue={this.state.searchValue}
           />
           {this.state.haveSearch &&
-            <ErrorMessage message="Sorry we could not find any recipe"/>
+            <ErrorMessage message="Sorry we could not find any recipe" />
+          }
+        </div>
+      );
+    } else { //else display all the recipe data that was fetched
+      return (
+        <div className={styles.appContainer}>
+          {this.state.searching ? //if the data is being fetch show the loading screen otherwise show all the recipe cards
+            < img src={loading} alt="Loading" />
+            :
+            <div>
+              <SeachBar
+                handleSubmit={this.handleSubmit.bind(this)}
+                handleChange={this.handleChange.bind(this)}
+                searchValue={this.state.searchValue}
+              />
+              <div className={styles.recipeContainer}>
+                {this.state.data.map(((recipe) => (
+                  <Recipe
+                    handleRecipeClick={this.handleRecipeClick.bind(this)}
+                    key={recipe.recipe.label}
+                    title={recipe.recipe.label}
+                    image={recipe.recipe.image}
+                    calories={recipe.recipe.calories}
+                    servings={recipe.recipe.yield}
+                    ingredients={recipe.recipe.ingredients}
+                    dietLabels={recipe.recipe.dietLabels}
+                    healthLabels={recipe.recipe.healthLabels}
+                  />
+                )))}
+              </div>
+              {this.state.showPopup && //if recipe card has been clicked, display more information in a popup box
+                <RecipePopup
+                  handleRecipeClick={this.handleRecipeClick.bind(this)}
+                  recipeData={this.state.recipeData}
+                />
+              }
+            </div>
           }
         </div>
       );
     }
   }
-
-  // render(){
-  //   return (
-  //     <div className={styles.appContainer}>
-  //       {this.state.data.length === 0 && this.state.searching === false ? /*if there is no recipe data and data is not currently being fetch then display landing page*/
-  //         <div>
-  //           <p className={styles.mainTitle}>Tonight's Recipe</p>
-  //           <p className={styles.subTitle}>Food ideas just a click away</p>
-  //           {/*image design by Amy Cleaver*/}
-  //           <img src={logo} className={styles.logo} alt="Logo"/>      
-  //           <SeachBar 
-  //             handleSubmit={this.handleSubmit.bind(this)}
-  //             handleChange={this.handleChange.bind(this)}
-  //             searchValue={this.state.searchValue}
-  //           />
-  //           {this.state.haveSearch &&
-  //             <div className={styles.error}>
-  //               <p className={styles.error}><b>Sorry we could not find any recipe</b></p>
-  //               {/*image was sourced from https://loading.io/*/}
-  //               <img src={error} alt="Error"/>
-  //             </div>
-  //           }
-  //         </div>
-  //         : /*else display recipe cards section*/
-  //         this.state.searching ? /*if the data is being fetch show the loading screen*/
-  //         <div className={styles.loading}>
-  //           {/*image was sourced from https://loading.io/*/}
-  //           <img src={loading} alt="Loading"/>
-  //         </div>
-  //         :/*else display the recipe cards section*/
-  //         <div>          
-  //           <SeachBar 
-  //             handleSubmit={this.handleSubmit.bind(this)}
-  //             handleChange={this.handleChange.bind(this)}
-  //             searchValue={this.state.searchValue}
-  //           />
-  //           <div className={styles.recipeContainer}>
-  //             {this.state.data.map(((recipe) => (
-  //               <Recipe
-  //                 handleRecipeClick={this.handleRecipeClick.bind(this)}
-  //                 key={recipe.recipe.label}
-  //                 title={recipe.recipe.label}
-  //                 image={recipe.recipe.image}
-  //                 calories={recipe.recipe.calories}
-  //                 servings={recipe.recipe.yield}
-  //                 ingredients={recipe.recipe.ingredients}
-  //                 dietLabels={recipe.recipe.dietLabels}
-  //                 healthLabels={recipe.recipe.healthLabels}
-  //               />
-  //             )))}
-  //           </div>
-  //         </div>          
-  //       }
-        
-  //       {this.state.showPopup && /*if recipe card has been clicked, display recipe popup data*/
-  //         <RecipePopup 
-  //           handleRecipeClick={this.handleRecipeClick.bind(this)}
-  //           recipeData={this.state.recipeData}  
-  //         />
-  //       }
-  //     </div>
-  //   );
-  // }
 }
